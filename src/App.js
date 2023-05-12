32db61c9ee91bd5a58586a113f5bf086ad61890a
rishavsagar4454 <rishavsagar4454@gmail.vom>
2023-05-12 18:34:58 +0530

    react-micro-project-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './index.css'
 function App() {
   return (
+    <div>
+      <div className="banner">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="section-left">
+      <h1><b>Inspiring design delivered to your inbox every morning</b></h1>
+      <h3>Our team scouts the internet for the best designs, illustrations and art and delivers a truly inspiring one every day to your inbox</h3>
+      <h2>Show me how it looks</h2>
+      
+      <div>    
+      <form>
+      <input type="email" placeholder="Your e-mail address" class="t5"/>
+      <button onclick=" myFunction"class="t6"> Register Now </button>
+       </form >
+       <div class="bottom-line">Free - No Spam - No Data Sharing</div>
+       
+     </div>
+     </div>
+     </div>
+     <div class="section-right">
+					<img src="../src/assests/Vector.png" alt='oldman_image' />
+				</div>
+     </div>
+     <div class="footer">
+				<ul>
+					<li>Prompt Generator</li>
+					<li>Dweep Daily</li>
+					<li>All Contributors</li>
+					<li>Your data on Dweep.io</li>
+					<li>Contribute to Dweep</li>
+				</ul>
+				<ul class="footer-list2">
+					<li>Dweep.io</li>
+					<li>Made with love in India</li>
+					<li class="logo-list">
+						<img src="src\assets\Vector.png" alt="insta"/>
+						<img src="./images/InstagramLogo-c295ba7b.svg" alt="linkedin" />
+					</li>
+					<li>&nbsp;</li>
+					<li>hello@dweep.io</li>
+				</ul>
+			</div>
     </div>
   );
 }
